93fd302328d9c69a030f288987e942bad60e126c
40891 <40891@longroad.ac.uk>
2014-10-02 14:49:50 +0100

    SR 2

---
+++ Selection Revision 2.py
@@ -0,0 +1,10 @@
+#Jeeson Baktha
+#Revision Excercise 2
+#1 October 2014
+
+age = int(input("Please enter your age to see if you are old enough to drive: "))
+
+if age >= 17:
+    print("You are old enough to drive.")
+else:
+    print("Sorry you are not old enough to drive.")
