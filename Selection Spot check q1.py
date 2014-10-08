920837a571583682290913043c614be3d2b954e8
40891 <40891@longroad.ac.uk>
2014-10-08 12:09:40 +0100

    SS check q1

---
+++ Selection Spot check q1.py
@@ -0,0 +1,10 @@
+#Jeeson Baktha
+#Selection Spot Check
+#8 October 2014
+
+attendance = float(input("Please enter your attendance as a number: "))
+
+if attendance <= 85:
+    print("Your attendance is only {0}%. You must improve your attendance".format(attendance))
+else:
+    print("Your attendance is {0}%. Keep up the good attendance.".format(attendance)) 
